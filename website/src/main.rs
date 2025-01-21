0903358ec2db53ce4db06035b00d0c4b8fe18e7d
VJMReichenbach <Valentin.Reichenbach@live.com>
2025-01-21 15:34:08 +0100

    moved more rendering into template

--- website/src/main.rs
+++ website/src/main.rs
@@ -11,12 +11,11 @@ fn index() -> Template {
 #[get("/custom-maps")]
 fn custom_maps() -> Template {
     let (maps, total_time, total_deaths) = parsing::parse_custom_maps();
-    let total_deaths_string = format!("Total deaths: {}", total_deaths);
     Template::render(
         "custom_maps",
         context! {
         total_time: total_time,
-        total_deaths: total_deaths_string,
+        total_deaths: total_deaths,
         map_table: maps
         },
     )
